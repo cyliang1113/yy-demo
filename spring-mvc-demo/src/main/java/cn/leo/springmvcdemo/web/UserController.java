cf25d0ceaa1ed9f142dc252f544de6bc7b585371
cyliang1113 <cyliang1113@126.com>
2016-11-14 15:44:50 +0800

    1111

---
+++ spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
@@ -0,0 +1,15 @@
+package cn.leo.springmvcdemo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/user/login")
+	@ResponseBody
+	public String login() {
+		return "hello";
+	}
+}
