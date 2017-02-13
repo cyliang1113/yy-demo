e8298d1f47839b92bf672794be953daa18e9785f
cyliang1113 <cyliang1113@126.com>
2017-02-13 19:08:28 +0800

    11

--- x-back/src/main/resources/x-back.sql
+++ x-back/src/main/resources/x-back.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 192.168.19.221
+Source Server         : 192.168.17.212
 Source Server Version : 50629
-Source Host           : 192.168.19.221:3306
-Source Database       : sys-back
+Source Host           : 192.168.17.212:3306
+Source Database       : x-back
 
 Target Server Type    : MYSQL
 Target Server Version : 50629
 File Encoding         : 65001
 
-Date: 2017-02-12 22:29:55
+Date: 2017-02-13 18:47:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,10 +28,15 @@ CREATE TABLE `t_sys_menu` (
   `menu_weight` int(11) DEFAULT NULL,
   `parent_menu_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_sys_menu
 -- ----------------------------
 INSERT INTO `t_sys_menu` VALUES ('1', '系统管理', '1', '1', null, '100', null);
-INSERT INTO `t_sys_menu` VALUES ('2', '菜单管理', '2', '2', '/sys/menu', '1', '1');
+INSERT INTO `t_sys_menu` VALUES ('2', '菜单管理', '2', '2', 'sysmanage/menumanage', '1', '1');
+INSERT INTO `t_sys_menu` VALUES ('3', '权限管理', '2', '1', null, '2', '1');
+INSERT INTO `t_sys_menu` VALUES ('4', '角色管理', '3', '2', 'sys/menu', '1', '3');
+INSERT INTO `t_sys_menu` VALUES ('5', '财务管理', '1', '1', null, '50', null);
+INSERT INTO `t_sys_menu` VALUES ('6', '支付监控', '2', '2', 'sys/menu', '50', '5');
+INSERT INTO `t_sys_menu` VALUES ('7', '退款监控', '2', '2', 'sys/menu', '51', '5');
