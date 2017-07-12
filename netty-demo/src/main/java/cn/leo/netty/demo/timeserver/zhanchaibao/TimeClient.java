ca6491a8259623bf9952d78faf4c9eda30aed002
cyliang1113 <cyliang1113@126.com>
2017-07-12 11:34:59 +0800

    11111

--- netty-demo/src/main/java/cn/leo/netty/demo/timeserver/zhanchaibao/TimeClient.java
+++ netty-demo/src/main/java/cn/leo/netty/demo/timeserver/zhanchaibao/TimeClient.java
@@ -1,7 +1,6 @@
 package cn.leo.netty.demo.timeserver.zhanchaibao;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
