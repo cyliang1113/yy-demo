5ce733bcdc83fdb25a2b2ee0cd75eeb320d397d2
cyliang1113 <cyliang1113@126.com>
2018-01-10 11:23:21 +0800

    11111

--- java-demo-maven/src/main/java/cn/leo/java/demo/log4j/Demo01.java
+++ java-demo-maven/src/main/java/cn/leo/java/demo/log4j/Demo01.java
@@ -5,6 +5,13 @@ import org.apache.log4j.Logger;
 public class Demo01 {
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Demo01.class);
+		
 		logger.info("hello");
+		
+		try{
+			int i = 1/0;
+		}catch(Exception e){
+			logger.info(e.getMessage(), e);
+		}
 	}
 }
