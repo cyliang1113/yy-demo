86ff08baae8e35cfe3ced1ca689de0cddd9a5c43
cyliang1113 <cyliang1113@126.com>
2018-04-03 10:35:43 +0800

    111111

--- java-demo-maven/src/main/java/cn/leo/java/demo/HelloWorld.java
+++ java-demo-maven/src/main/java/cn/leo/java/demo/HelloWorld.java
@@ -1,12 +1,7 @@
 package cn.leo.java.demo;
 
-import java.util.HashMap;
-
 public class HelloWorld {
 	public static void main(String[] args) {
-		HashMap<Object,Object> hashMap = new HashMap<>();
-		hashMap.put("dsfd", "1.111");
-		String aa = "1.111";
-		Double a = (Double) (hashMap.get("dsfd"));
+
 	}
 }
