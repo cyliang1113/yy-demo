fb77006a639090678d83ea830c2f2a145f8d7b50
cyliang1113 <cyliang1113@126.com>
2018-04-03 10:51:15 +0800

    11111

--- java-demo-maven/src/main/java/cn/leo/java/demo/HelloWorld.java
+++ java-demo-maven/src/main/java/cn/leo/java/demo/HelloWorld.java
@@ -2,6 +2,7 @@ package cn.leo.java.demo;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println(11 > 2);
+		System.out.println(11 >> 1);
+		System.out.println(7 >> 1);
 	}
 }
