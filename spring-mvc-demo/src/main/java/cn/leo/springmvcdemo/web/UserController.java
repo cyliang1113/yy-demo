cd2b8433580300eb074239d8d03d7badf01dd534
cyliang1113 <cyliang1113@126.com>
2018-04-16 16:42:09 +0800

    springmvc-demo

--- spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
+++ spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
@@ -2,13 +2,20 @@ package cn.leo.springmvcdemo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
 	@RequestMapping("/loginpage")
-	public String login() {
+	public String loginPage() {
 		return "loginpage";
+	}
+	
+	@RequestMapping("/loginout")
+	@ResponseBody
+	public String loginout() {
+		return "loginout";
 	}
 }
