bf0cbdbb26ba9ae1223593d2e407f577901903d6
cyliang1113 <cyliang1113@126.com>
2018-04-20 17:22:52 +0800

    123

--- spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
+++ spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
@@ -1,5 +1,6 @@
 package cn.leo.springmvcdemo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserServiceImpl userServiceImpl;
 
 	@RequestMapping("/loginpage")
 	public String loginPage() {
+		userServiceImpl.getUsername();
 		return "loginpage";
 	}
 	
---
+++ spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.leo.springmvcdemo.web;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceImpl {
+	public String getUsername(){
+		return "xixi";
+	}
+}
