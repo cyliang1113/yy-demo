a4bd5911c3a9add6bca232525d4e6431e267ac29
cyliang1113 <cyliang1113@126.com>
2018-04-28 21:59:44 +0800

    idea

---
+++ java-demo-maven/src/main/java/cn/leo/java/demo/meiju/Demo01.java
@@ -0,0 +1,16 @@
+package cn.leo.java.demo.meiju;
+
+/**
+ * 
+ * @author leo
+ *
+ */
+public class Demo01 {
+	public static void main(String[] args) {
+		System.out.println(Status.TURE);
+	}
+	
+	private static enum Status{
+		TURE, FALSE;
+	}
+}
