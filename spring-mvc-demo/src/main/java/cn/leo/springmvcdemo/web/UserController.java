fe2e28d24ae4ab9322cb13460cb55c678c7b2690
cyliang1113 <cyliang1113@126.com>
2018-06-04 07:59:28 +0800

    1111

--- spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
+++ spring-mvc-demo/src/main/java/cn/leo/springmvcdemo/web/UserController.java
@@ -1,13 +1,20 @@
 package cn.leo.springmvcdemo.web;
 
+import java.math.BigDecimal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	private final Log logger = LogFactory.getLog(getClass());
+	
 	@Autowired
 	private UserServiceImpl userServiceImpl;
 
@@ -19,7 +26,14 @@ public class UserController {
 	
 	@RequestMapping("/loginout")
 	@ResponseBody
-	public String loginout() {
+	public String loginout(
+		@RequestParam(value = "age", required = false) Integer age,
+		@RequestParam(value = "name", required = false) String name,
+		@RequestParam(value = "price", required = false) BigDecimal price
+	) {
+		logger.info("age:" + age);
+		logger.info("name:" + name);
+		logger.info("price:" + price);
 		return "loginout";
 	}
 }
