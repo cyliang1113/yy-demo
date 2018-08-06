c610b943ac3e86a90aef2e6c9076f5e625694a18
cyliang1113 <cyliang1113@126.com>
2018-08-06 17:59:29 +0800

    mybatis log

--- mybatis-demo/src/main/java/cn/leo/mybatisdemo/Test.java
+++ mybatis-demo/src/main/java/cn/leo/mybatisdemo/Test.java
@@ -11,21 +11,21 @@ public class Test {
 	private static final Log logger = LogFactory.getLog(Test.class);
 	
 	public static void main(String[] args) {
-//		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-//		UserUserService service = (UserUserService) ac.getBean("userUserService");
-//
-//		UserUser userUser = service.findByUserId(1L);
-//
-//		logger.info(userUser);
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserUserService service = (UserUserService) ac.getBean("userUserService");
+
+		UserUser userUser = service.findByUserId(1L);
+
+		logger.info(userUser);
 		
 //		UserUser userUser2 = new UserUser();
 //		userUser2.setUsername("yoo");
 //		userUser2.setCellphone("15166668899");
 //		service.saveUser(userUser2);
-		
-		Object userUser2 = new Object();
-		logger.info(userUser2);
-		logger.info(userUser2.hashCode());
+//
+//		Object userUser2 = new Object();
+//		logger.info(userUser2);
+//		logger.info(userUser2.hashCode());
 
 	}
 }
